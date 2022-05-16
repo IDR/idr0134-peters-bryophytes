a8c6c1ac9b5eea807dc782472751f6593220ce08
William Moore <w.moore@dundee.ac.uk>
2022-05-16 13:29:42 +0100

    re-ran filePaths.py from allFiles.txt

--- scripts/filePaths.py
+++ scripts/filePaths.py
@@ -4,16 +4,19 @@ import csv
 rows = []
 
 base_dir = "/uod/idr/filesets/idr0134-peters-bryophytes/20211214-Globus/"
+project = "Project:name:idr0134-peters-bryophytes/experimentA/"
 
 # with open("experimentA/idr0134-experimentA-filePaths.tsv") as file:
-with open("experimentA/idr0134-experimentA-filePaths.tsv") as csvfile:
+with open("allFiles.txt") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         line = row[0]
         print("line", line)
+        if ".DS_Store" in line:
+            continue
         path = line.replace(base_dir, "")
         ds_name = path.split("/")[0]
-        rows.append(["Dataset:name:" + ds_name, line])
+        rows.append([project + "Dataset:name:" + ds_name, line])
 
 # write tsv
 with open('idr0134-experimentA-filePaths2.tsv', 'w') as tsvfile:
