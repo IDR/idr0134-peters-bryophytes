d229b6f6b4d8c3fdee027e2e10fe47c4f9b46d83
William Moore <w.moore@dundee.ac.uk>
2022-06-07 11:43:37 +0100

    Update scripts/filePaths_remove_jpeg.py to process allFiles.txt

--- scripts/filePaths_remove_jpeg.py
+++ scripts/filePaths_remove_jpeg.py
@@ -7,28 +7,39 @@ import csv
 imagePaths = set()
 
 # Read all image paths...
-with open("experimentA/idr0134-experimentA-filePaths.tsv") as inputFile:
-    reader = csv.reader(inputFile, delimiter='\t')
-    for row in reader:
-        imgPath = row[1]
-        imagePaths.add(imgPath)
+with open("allFiles.txt") as inputFile:
+    imagePaths = set([line.strip() for line in inputFile.readlines()])
+
 print("Read filenames", len(imagePaths))
 
+GLOBUS_DIR = "20211214-Globus"
+FTP_DIR = "20220531-ftp"
+
 # read again, removing .tiff and .jpeg ONLY if .ome.tiff exists
-with open("experimentA/idr0134-experimentA-filePaths.tsv") as inputFile:
-    with open('idr0134-experimentA-filePaths2.tsv', 'w') as tsvfile:
-        reader = csv.reader(inputFile, delimiter='\t')
-        writer = csv.writer(tsvfile, delimiter='\t')
-        for row in reader:
-            imgPath = row[1]
-            ignore = False
-            for ext in [".jpeg", ".tiff"]:
-                if imgPath.endswith(ext) and not imgPath.endswith(".ome.tiff"):
-                    print("imgPath", imgPath)
-                    if imgPath.replace(ext, ".ome.tiff") in imagePaths:
-                        # can ignore
-                        print("IGNORE -------")
-                        ignore = True
-            if not ignore:    
-                # write
-                writer.writerow(row)
+with open("allFiles.txt") as inputFile:
+    with open('noJpegTiffDuplicates7thJune.txt', 'w') as outFile:
+        for imgPath in inputFile.readlines():
+            imgPath = imgPath.strip()
+            # remove hidden files
+            if "/." in imgPath:
+                continue
+            # remove .jpeg if corresponding .tiff or .ome.tiff is found
+            if imgPath.endswith(".jpeg"):
+                ome_path = imgPath.replace(".jpeg", ".ome.tiff")
+                tiff_path = imgPath.replace(".jpeg", ".tiff")
+                if ome_path in imagePaths or tiff_path in imagePaths:
+                    continue
+                # Also check in later ftp dir...
+                if tiff_path.replace(GLOBUS_DIR, FTP_DIR) in imagePaths or ome_path.replace(GLOBUS_DIR, FTP_DIR) in imagePaths:
+                    continue
+                
+            if imgPath.endswith(".tiff") and not imgPath.endswith(".ome.tiff"):
+                tiff_path = imgPath.replace(".tiff", ".ome.tiff")
+                if tiff_path in imagePaths or tiff_path.replace(GLOBUS_DIR, FTP_DIR) in imagePaths:
+                    continue
+            
+            if imgPath.endswith(".TIF") and not imgPath.endswith(".ome.tiff"):
+                tiff_path = imgPath.replace(".TIF", ".ome.tiff")
+                if tiff_path in imagePaths or tiff_path.replace(GLOBUS_DIR, FTP_DIR) in imagePaths:
+                    continue
+            outFile.write(imgPath + "\n")
