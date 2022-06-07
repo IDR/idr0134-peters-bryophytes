4c619b66aeef63498af10f1c84707fb624862f6e
William Moore <w.moore@dundee.ac.uk>
2022-06-07 11:57:04 +0100

    Update filePaths.py to also use ftp data. New filePaths.tsv

--- scripts/filePaths.py
+++ scripts/filePaths.py
@@ -1,25 +1,27 @@
 
 import csv
+from ftplib import ftpcp
 
 rows = []
 
-base_dir = "/uod/idr/filesets/idr0134-peters-bryophytes/20211214-Globus/"
+base_dir = "/uod/idr/filesets/idr0134-peters-bryophytes/"
+globas_dir = "20211214-Globus/"
+ftp_dir = "20220531-ftp/"
 project = "Project:name:idr0134-peters-bryophytes/experimentA/"
 
-# with open("experimentA/idr0134-experimentA-filePaths.tsv") as file:
-with open("allFiles.txt") as csvfile:
+with open("noJpegTiffDuplicates7thJune.txt") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         line = row[0]
         print("line", line)
-        if ".DS_Store" in line:
+        if not ".ome.tiff" in line:
             continue
-        path = line.replace(base_dir, "")
+        path = line.replace(base_dir, "").replace(globas_dir, "").replace(ftp_dir, "")
         ds_name = path.split("/")[0]
         rows.append([project + "Dataset:name:" + ds_name, line])
 
 # write tsv
-with open('idr0134-experimentA-filePaths2.tsv', 'w') as tsvfile:
+with open('idr0134-experimentA-filePaths.tsv', 'w') as tsvfile:
     writer = csv.writer(tsvfile, delimiter='\t')
     for row in rows:
         writer.writerow(row)
