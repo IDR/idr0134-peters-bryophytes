8a4c42b041330a5ea6247b53ed876cd8f3d0d974
William Moore <w.moore@dundee.ac.uk>
2022-06-09 13:50:53 +0100

    Update filePaths.py and filePaths.tsv with re-uploaded image

--- scripts/filePaths.py
+++ scripts/filePaths.py
@@ -9,6 +9,10 @@ globas_dir = "20211214-Globus/"
 ftp_dir = "20220531-ftp/"
 project = "Project:name:idr0134-peters-bryophytes/experimentA/"
 
+# 3 files were re-uploaded (ftp failed), but the other 2 were already in Globus upload
+reuploaded_img = "IMG_9021 Scapania aequiloba stature dorsal side (MP-E 2x).ome.tiff"
+reuploaded_dir = "20220608-ftp/"
+
 img_keys = set()
 duplicates = 0
 with open("noJpegTiffDuplicates7thJune.txt") as csvfile:
@@ -22,6 +26,8 @@ with open("noJpegTiffDuplicates7thJune.txt") as csvfile:
         ds_name = path.split("/")[0].replace("spitzbergensis", "spitsbergensis")
         img_name = path.split("/")[-1].replace("spitzbergensis", "spitsbergensis")
         img_key = f'{ds_name},{img_name}'
+        if img_name == reuploaded_img:
+            line = line.replace(ftp_dir, reuploaded_dir)
         if img_key not in img_keys:
             rows.append([project + "Dataset:name:" + ds_name, line, img_name])
             img_keys.add(img_key)
