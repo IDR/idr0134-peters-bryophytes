8ce906f9850142725e36857854fac5566ef02500
William Moore <w.moore@dundee.ac.uk>
2022-06-09 12:20:10 +0100

    filePaths.py removes duplicates from filePaths.tsv

--- scripts/filePaths.py
+++ scripts/filePaths.py
@@ -9,18 +9,27 @@ globas_dir = "20211214-Globus/"
 ftp_dir = "20220531-ftp/"
 project = "Project:name:idr0134-peters-bryophytes/experimentA/"
 
+img_keys = set()
+duplicates = 0
 with open("noJpegTiffDuplicates7thJune.txt") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         line = row[0]
-        print("line", line)
         if not ".ome.tiff" in line:
             continue
         path = line.replace(base_dir, "").replace(globas_dir, "").replace(ftp_dir, "")
         # Fix "spitzbergensis" -> "spitsbergensis"
         ds_name = path.split("/")[0].replace("spitzbergensis", "spitsbergensis")
         img_name = path.split("/")[-1].replace("spitzbergensis", "spitsbergensis")
-        rows.append([project + "Dataset:name:" + ds_name, line, img_name])
+        img_key = f'{ds_name},{img_name}'
+        if img_key not in img_keys:
+            rows.append([project + "Dataset:name:" + ds_name, line, img_name])
+            img_keys.add(img_key)
+        else:
+            print("Duplicate", line)
+            duplicates += 1
+
+print("Duplicates: ", duplicates)
 
 # write tsv
 with open('idr0134-experimentA-filePaths.tsv', 'w') as tsvfile:
