7c685cca714186dcc3da7b2adcd0eaa2005181aa
William Moore <w.moore@dundee.ac.uk>
2022-06-27 11:33:48 +0100

    Final fixes to filePaths.py and filePaths.tsv

--- scripts/filePaths.py
+++ scripts/filePaths.py
@@ -1,6 +1,5 @@
 
 import csv
-from ftplib import ftpcp
 
 rows = []
 
@@ -9,14 +8,20 @@ globas_dir = "20211214-Globus/"
 ftp_dir = "20220531-ftp/"
 project = "Project:name:idr0134-peters-bryophytes/experimentA/"
 
-# 3 files were re-uploaded (ftp failed), but the other 2 were already in Globus upload
-reuploaded_img = "IMG_9021 Scapania aequiloba stature dorsal side (MP-E 2x).ome.tiff"
-reuploaded_dir = "20220608-ftp/"
+# 5 more uploaded "20220624-ftp" - NB: see below...
+ftp_20220624 = [
+    "IMG_9021 Scapania aequiloba stature dorsal side (MP-E 2x).ome.tiff",
+    "IMG_3568-3667 Diplophyllum taxifolium antical lobe basal leaf margin (40x).ome.tiff",
+    "IMG_3368-3467 Diplophyllum taxifolium postical lobe leaf base (40x).ome.tiff",
+    "IMG_3245-3344 Diplophyllum taxifolium postical lobe leaf center (40x).ome.tiff",
+    "IMG_2497-2596 Douinia ovata postical lobe leaf center (40x).ome.tiff"
+]
 
 typos = [
     ["frisch", "fresh"],
     ["spitzbergensis", "spitsbergensis"],
-    ["scapania_ hyperborea", "scapania_hyperborea"]
+    ["scapania_ hyperborea", "scapania_hyperborea"],
+    ["stature_oberunterseite", "stature_dorsal_and_ventral_sides"]
 ]
 def fix_typos(text):
     for typo in typos:
@@ -36,8 +41,10 @@ with open("noJpegTiffDuplicates7thJune.txt") as csvfile:
         ds_name = fix_typos(path.split("/")[0])
         img_name = fix_typos(path.split("/")[-1])
         img_key = f'{ds_name},{img_name}'
-        if img_name == reuploaded_img:
-            line = line.replace(ftp_dir, reuploaded_dir)
+        if img_name in ftp_20220624:
+            line = line.replace(globas_dir, "20220624-ftp")
+            line = line.replace(ftp_dir, "20220624-ftp")
+            # NB: these lines needed manual editing after this, to remove /dir in  "20220624-ftp/dir"
         if img_key not in img_keys:
             rows.append([project + "Dataset:name:" + ds_name, line, img_name])
             img_keys.add(img_key)
